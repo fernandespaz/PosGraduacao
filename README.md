ebf6d791f3699198602f985587879f86280d56e9
c1313769 DiegoFernandes <c1313769@interno.bb.com.br>
2020-05-14 16:59:19 -0300

    React Native1

---
+++ README.md
@@ -0,0 +1,9 @@
+ToDoList
+
+"version": "1.0.0"
+
+Function;
+Created a new task
+
+* Run NPM or Yarn install 
+* npx react-native run-android
---
+++ index.js
@@ -0,0 +1,22 @@
+/* eslint-disable react-native/no-inline-styles */
+import 'react-native-gesture-handler';
+import {initializeFirebaseApi} from './src/services/FirebaseApi';
+import React from 'react';
+import {AppRegistry, SafeAreaView} from 'react-native';
+import Routes from './src/routes/Routes';
+import {name as appName} from './app.json';
+import {NavigationContainer} from '@react-navigation/native';
+
+const wrappedRoutes = () => {
+  return (
+    <NavigationContainer>
+      <SafeAreaView style={{flex: 1}}>
+        <Routes />
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+};
+AppRegistry.registerComponent(appName, () => {
+  initializeFirebaseApi();
+  return wrappedRoutes;
+});
